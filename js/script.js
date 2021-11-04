609de7de90ddb0d25b3ca133bc03b018148e5645
Serhii Sapiannyk <serhii09@icloud.com>
2021-11-04 14:58:53 +0100

    Add printMassage(papier)

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let computerMOve = `kamień`;
+let playerMove = `papier`;
+
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem' + playerMove + '!Jeśli Twój ruch to nozczki, to wygrywasz!');
