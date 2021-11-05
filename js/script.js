bd5976797a34f5a47e9466accec7ab49616433e0
Serhii Sapiannyk <serhii09@icloud.com>
2021-11-05 13:33:18 +0100

    Add else if computerMove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-let computerMOve = `kamień`;
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem' + playerMove + '!Jeśli Twój ruch to nozyczki, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+    computerMove = `papier`
+}
+else if(randomNumber == 3){
+    computerMove = `nozyczki`
+}
+printMessage('Mój ruch to: ' + computerMove);
