cc8b257ae601da969847c39a2c4113926aa8479a
Serhii Sapiannyk <serhii09@icloud.com>
2021-11-05 11:17:33 +0100

    Fix functions.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ let playerMove = `papier`;
 
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem' + playerMove + '!Jeśli Twój ruch to nozczki, to wygrywasz!');
+printMessage('Zagrałem' + playerMove + '!Jeśli Twój ruch to nozyczki, to wygrywasz!');
