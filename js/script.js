4c1b73cfac81283143a6586dc41af2de86d2cdc5
Serhii Sapiannyk <serhii09@icloud.com>
2021-11-12 12:47:23 +0100

    Add const computer/random/player

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ function playGame(playerInput){
         clearMessages();
     
 // COMPUTER
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
 
         console.log('Wylosowana liczba to: ' + randomNumber);
 
-    let computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -18,7 +18,7 @@ printMessage('Mój ruch to: ' + computerMove);
     
     console.log('Gracz wpisał: ' + playerInput);
 
-    let playerMove = getMoveName(playerInput);
+    const playerMove = getMoveName(playerInput);
 
     function getMoveName( playerMove){
         if( playerMove == 1){
